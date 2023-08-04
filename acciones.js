d95973ce1944d3bfbceaaa26ea5298c04f16984a
Franco Nuñez <franconunez700@gmail.com>
2023-08-04 15:12:53 -0300

    prim

---
+++ acciones.js
@@ -0,0 +1,16 @@
+function ope(num){
+    document.calculadora.texto.value=num; 
+} 
+
+function calcu(calcus) {
+    NumEspera = String; 
+
+    NumEspera = document.calculadora.texto.value;
+
+    document.calculadora.texto.value += NumEspera; 
+
+}
+
+function restultado(resul) {
+
+}
